7378fc44d7be49da712c19512cb52c4b825d3931
Maxim Eroshenko <eroshenko.maksim.87@mail.ru>
2020-08-11 21:22:40 +0300

    Исправление аргументов

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,5 @@
 public class BmiService {
     public int calculate(double weigth, int growth) {
-        weigth = 1.76;
-        growth = 95;
         double bmi = growth / (weigth * weigth);
         System.out.println(bmi);
 
